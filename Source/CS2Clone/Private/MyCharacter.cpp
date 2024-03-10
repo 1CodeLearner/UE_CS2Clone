96f34aa3eb679ca6bcaf2320ff5a735f031f1967
carttman <93562297+carttman@users.noreply.github.com>
2024-03-10 18:06:58 +0900

    Third Template, player 이동, 점프, 회전, gameMode 추가

---
+++ Source/CS2Clone/Private/MyCharacter.cpp
@@ -0,0 +1,80 @@
+#include "MyCharacter.h"
+#include <../../../../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputSubsystems.h>
+#include <../../../../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputComponent.h>
+#include <../../../../../../../Source/Runtime/Engine/Classes/Engine/SkeletalMesh.h>
+
+// Sets default values
+AMyCharacter::AMyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> tempMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/Mannequins/Meshes/SKM_Manny.SKM_Manny'"));
+	if (tempMesh.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(tempMesh.Object);
+	}
+
+	GetMesh()->SetRelativeLocation(FVector(0, 0, -88));
+	GetMesh()->SetRelativeRotation(FRotator(0, -90, 0));
+}
+
+// Called when the game starts or when spawned
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	//get APlayerController
+	APlayerController* playerContoller = Cast<APlayerController>(GetController());
+	//get subSystem
+	UEnhancedInputLocalPlayerSubsystem* subSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(playerContoller->GetLocalPlayer());
+	subSystem->AddMappingContext(imc_Default, 0);
+
+}
+
+// Called every frame
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* enhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if(enhancedInputComponent != nullptr)
+	{
+		enhancedInputComponent->BindAction(ia_Move, ETriggerEvent::Triggered, this, &AMyCharacter::EnhancedMove);
+		//enhancedInputComponent->BindAction(ia_Move, ETriggerEvent::Completed, this, &AMyCharacter::EnhancedMove);
+		enhancedInputComponent->BindAction(ia_Jump, ETriggerEvent::Triggered, this, &AMyCharacter::EnhancedJump);
+		enhancedInputComponent->BindAction(ia_Look, ETriggerEvent::Triggered, this, &AMyCharacter::EnhancedLook);
+	}
+}
+
+void AMyCharacter::EnhancedMove(const struct FInputActionValue& value)
+{	
+	FVector2D dir = value.Get<FVector2D>();
+	FVector originVec = FVector(dir.Y, dir.X, 0);
+	FVector newVec = GetTransform().TransformVector(originVec);
+
+	AddMovementInput(newVec);
+}
+
+void AMyCharacter::EnhancedJump(const struct FInputActionValue& value)
+{
+	UE_LOG(LogTemp, Warning, TEXT("jump"));
+	Jump();
+}
+
+void AMyCharacter::EnhancedLook(const struct FInputActionValue& value)
+{
+	FVector2D dir = value.Get<FVector2D>();
+
+	AddControllerYawInput(dir.X);
+	AddControllerPitchInput(dir.Y);
+
+}
+
